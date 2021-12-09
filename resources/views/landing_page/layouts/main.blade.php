3757014990f8db949db82cb838ae3fe3561edd38
idika87 <anhdikaparmana87@gmail.com>
2021-12-09 15:16:26 +0800

    penambahan di landingpage

--- resources/views/landing_page/layouts/main.blade.php
+++ resources/views/landing_page/layouts/main.blade.php
@@ -1,10 +1,11 @@
 <main id="main">
 
-  @include('landing_page/pages/prodak')
-  @include('landing_page/pages/client')
-  @include('landing_page/pages/team')
-  @include('landing_page/pages/tamplate')
-  @include('landing_page/pages/bonus')
-  @include('landing_page/pages/contact')
+	@include('landing_page/pages/about')
+	@include('landing_page/pages/prodak')
+	@include('landing_page/pages/client')
+	@include('landing_page/pages/team')
+	@include('landing_page/pages/tamplate')
+	@include('landing_page/pages/bonus')
+	@include('landing_page/pages/contact')
 
   </main><!-- End #main -->
--- resources/views/landing_page/pages/about.blade.php
+++ resources/views/landing_page/pages/about.blade.php
@@ -3,30 +3,30 @@
 	<div class="container-fluid">
 
 		<div class="row">
-			<div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-stretch" data-aos="fade-right">
-				<a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
+			<div class="col-xl-5 col-lg-6 d-flex justify-content-center align-items-stretch" data-aos="fade-right">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3944.189518133077!2d115.20861131433655!3d-8.673519690691522!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd240ea77d31a0d%3A0x1917f8a9d8663d74!2sBALI%20INTERNASIONAL%20TEKNOLOGI!5e0!3m2!1sid!2sid!4v1602766234217!5m2!1sid!2sid" width="1200" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 			</div>
 
 			<div class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5" data-aos="fade-left">
-				<h3>Enim quis est voluptatibus aliquid consequatur fugiat</h3>
-				<p>Esse voluptas cumque vel exercitationem. Reiciendis est hic accusamus. Non ipsam et sed minima temporibus laudantium. Soluta voluptate sed facere corporis dolores excepturi. Libero laboriosam sint et id nulla tenetur. Suscipit aut voluptate.</p>
+				<h3>PT. Bali Internasional Teknologi</h3>
+				<a>Jl. Halmahera No.47A, Dauh Puri Klod, Kec. Denpasar Barat, Kota Denpasar, Provinsi Bali Phone (0361) – 4748222</a>
 
 				<div class="icon-box" data-aos="zoom-in" data-aos-delay="100">
-					<div class="icon"><i class="bx bx-fingerprint"></i></div>
-					<h4 class="title"><a href="">Lorem Ipsum</a></h4>
-					<p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+					<p class="description">PT. Bali Internasional Teknologi merupakan alat dari Inovasi dan Kreatifitas yang memberikan nilai kepada pelanggan dalam mencapai tujuannya melalui Teknologi Informasi untuk mewujudkan sistem yang terintegrasi dengan mengimplementasikan Aplikasi Bisnis dan Aplikasi Pendukung serta IT Konsultasi, Layanan Profesional di seluruh komunitas bisnis Indonesia untuk Industri Perbankan, Jasa Keuangan, Komersial dan Sektor Pemerintah serta berbagai sektor industri lainnya.</p>
 				</div>
 
 				<div class="icon-box" data-aos="zoom-in" data-aos-delay="200">
-					<div class="icon"><i class="bx bx-gift"></i></div>
-					<h4 class="title"><a href="">Nemo Enim</a></h4>
-					<p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+					<p class="description">PT. Bali Internasional Teknologi salah satu perusahaan IT yang lahir di Bali dengan Visi “Menjadi salah satu perusahaan terbaik penyedia solusi berbasis Teknologi Informasi di dalam negeri maupun di luar negeri” dan misi “Meningkatkan produktivitas dan profitabilitas Client dengan solusi Teknologi Informasi”dengan value “Integritas, Kepercayaan dan Kopetensi”</p>
 				</div>
 
 				<div class="icon-box" data-aos="zoom-in" data-aos-delay="300">
-					<div class="icon"><i class="bx bx-atom"></i></div>
-					<h4 class="title"><a href="">Dine Pad</a></h4>
-					<p class="description">Explicabo est voluptatum asperiores consequatur magnam. Et veritatis odit. Sunt aut deserunt minus aut eligendi omnis</p>
+					<p class="description">Sebagai perusahaan IT yang leading di bidang Sektor Finansial dan Non Finansial PT. Bali Internasional Teknologi mempunyai filosofi memberikan solusi bedasarkan keinginan Kustomer, dengan meningkatnya produktivitas, profitabilitas dan kepuasan kustomer sebagai investasi yang sangat berharga bagi PT. Bali Internasional Teknologi.</p>
+				</div>
+
+				<div class="icon-box" data-aos="zoom-in" data-aos-delay="300">
+					<p class="description">PT. Bali Internasional Teknologi tidak memberikan produk IT yang baku tapi PT. Bali Internasional Teknologi memberikan solusi bedasarkan keinginan Pelanggan "dengan meningkatnya produktivitas, profitabilitas dan kepuasan pelanggan adalah investasi yang sangat berharga bagi kami". PT. Bali Internasional Teknologi mempunyai tenaga ahli yang spesialis di bidangnya dan merupakan lulusan dalam negri maupun luar negri. Bedasarkan survey, pelanggan yang pernah bermitra dengan kami sangat puas atas support yang telah kami berikan.
+						“Don’t Say It Possible to Create a Technology Because We Deliver What Customers Want ...! And Never Say Maybe for Technology...! We Will Make It Happen for You
+					</p>
 				</div>
 
 			</div>
